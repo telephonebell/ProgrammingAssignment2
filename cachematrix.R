e4fe10c94e7083131ad92e45ce556f94e6ccaee2
telephonebell <capitolmotion@gmail.com>
2017-01-29 22:05:45 -0800

    R Programming Assignment 2 submission

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,33 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Both these functions work together to produce inverted-matrix outputs for a series of 
+## input matrices such that eliminates redundant operations. 
 
-## Write a short comment describing this function
+
+
+## This function takes a matrix object as input and caches the result in a variable, tmp.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  
+  tmp <<- x
+  return(tmp)
 }
 
 
-## Write a short comment describing this function
+## This function takes a matrix as input and returns the inverse of the matrix. 
+## If inverse of a matrix already has been computed, a cached result is returned instead.
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+tmp_in = matrix()
+
+cacheSolve <- function(x) {
+        
+  if(!identical(x, tmp_in)){
+    tmp_in <<- x
+    return(makeCacheMatrix(solve(x)))
+  }
+        
+  else {
+    return(tmp) 
+  }
 }
+
+
+
